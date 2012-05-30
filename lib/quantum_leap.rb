1fdb8969925694bfc53a3e79ecf8596ee798b744
Matthew Conway <himself@mattonrails.com>
2012-05-30 12:42:53 -0400

    Allow returning to the real present time

--- lib/quantum_leap.rb
+++ lib/quantum_leap.rb
@@ -7,6 +7,10 @@ module QuantumLeap
     @time_travel_offsets ||= []
   end
 
+  def reset
+    @time_travel_offsets = []
+  end
+
   def mock_current_time(new_time)
     time_travel_offsets.push(Time.now - new_time)
   end
@@ -24,5 +28,9 @@ end
 class Quantum
   def self.leap(time)
     QuantumLeap.mock_current_time(time)
+  end
+
+  def self.leap_back
+    QuantumLeap.reset
   end
 end
--- spec/quantum_spec.rb
+++ spec/quantum_spec.rb
@@ -12,4 +12,16 @@ describe Quantum do
     end
   end
 
+  describe '.leap_back' do
+    the_present = Time.new
+    the_past    = Time.new(1972, 6, 15, 15, 00)
+
+    it 'returns to the present time' do
+      Quantum.leap(the_past)
+      Time.now.must_be_within_delta(the_past, 1)
+      Quantum.leap_back
+      Time.now.must_be_within_delta(the_present, 1)
+    end
+  end
+
 end
