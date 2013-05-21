b84b81ad00e17d537f6daae031ba9c008f493b5c
Matthew Conway <himself@mattonrails.com>
2013-05-20 23:12:10 -0700

    Document Quantum and QuantumLeap
    - Closes mattonrails/quantum_leap#6

--- lib/quantum.rb
+++ lib/quantum.rb
@@ -1,8 +1,38 @@
 require 'quantum_leap'
 
+# Public: Quantum is the public-facing interface to the library.
+#
+# Examples
+#
+#   Quantum.leap(Time.new(1956, 9, 13))
+#   # => 1956-09-13 00:00:00 -0700
+#
+#   Quantum.leap_back
+#   # => 2013-05-20 21:55:54 -0700
+#
+#   Quantum.leap(Time.new(1974, 10, 24)) do
+#     puts Time.now
+#   end
 class Quantum
+  # Internal: Default time to replace the real current time with.
   INITIAL_LEAP = Time.new(1956, 9, 13, 15, 00)
 
+  # Public: Change the current time to a specific point in the past or future.
+  # You can change the time temporarily by specifying a block; time is
+  # unchanged outside the context of the block.
+  #
+  # time - The Time to leap to (default: 1956-09-13 00:00:00).
+  #
+  # Examples
+  #
+  #   Quantum.leap(Time.new(1956, 9, 13))
+  #   # => 1956-09-13 00:00:00 -0700
+  #
+  #   Quantum.leap(Time.new(1974, 10, 24)) do
+  #     puts Time.now
+  #   end
+  #
+  # Returns the current time.
   def self.leap(time = INITIAL_LEAP)
     QuantumLeap::Accelerator.mock_current_time(time)
     if block_given?
@@ -15,6 +45,9 @@ class Quantum
     return Time.now
   end
 
+  # Public: Return to the present time.
+  #
+  # Returns the current time.
   def self.leap_back
     QuantumLeap::Accelerator.reset
     Time.now
--- lib/quantum_leap/accelerator.rb
+++ lib/quantum_leap/accelerator.rb
@@ -1,25 +1,87 @@
 require 'quantum_leap'
 
 module QuantumLeap
+  # Internal: The main module for methods that manipulate time, and provide
+  # interfaces to the custom time.
+  #
+  # All methods are module methods and should be called on the
+  # QuantumLeap::Accelerator module.
+  #
+  # Examples
+  #
+  #   QuantumLeap::Accelerator.mock_current_time(Time.new(1956, 9, 13))
+  #   # => [1788820900.589824]
+  #
+  #   QuantumLeap::Accelerator.time_travel_offsets
+  #   # => [1788820900.589824]
+  #
+  #   QuantumLeap::Accelerator.now
+  #   # => 1956-09-13 00:00:01 -0700
+  #
+  #   QuantumLeap::Accelerator.date
+  #   # => #<Date: 1956-09-13 ((2435730j,0s,0n),+0s,2299161j)>
+  #
+  #   QuantumLeap::Accelerator.reset
+  #   # => []
   module Accelerator
     extend self
 
+    # Internal: Return or initialize the stack of time offsets as Floats.
+    #
+    # Examples
+    #
+    #   QuantumLeap::Accelerator.time_travel_offsets
+    #   # => [1788820900.589824]
+    #
+    # Returns an empty Array or an Array of Floats.
     def time_travel_offsets
       @time_travel_offsets ||= []
     end
 
+    # Internal: Empties the stack of time offsets, effectively resetting the
+    # current time to the actual time.
+    #
+    # Returns an empty Array.
     def reset
       @time_travel_offsets = []
     end
 
+    # Internal: Add to the stack of time offsets the difference between the
+    # actual current time, and the desired new time.
+    #
+    # new_time - The past or future Time to leap to.
+    #
+    # Examples
+    #
+    #   QuantumLeap::Accelerator.mock_current_time(Time.new(1956, 9, 13))
+    #   # => [1788820900.589824]
+    #
+    # Returns the stack of time offsets, an Array of Floats.
     def mock_current_time(new_time)
       time_travel_offsets.push(Time.now - new_time)
     end
 
+    # Internal: Provide the mocked time by applying all offsets to the actual
+    # current time.
+    #
+    # Examples
+    #
+    #   QuantumLeap::Accelerator.now
+    #   # => 1956-09-13 00:00:01 -0700
+    #
+    # Returns the mocked current Time.
     def now
       Time.really_now - time_travel_offsets.inject(0, :+)
     end
 
+    # Internal: Converts the current mocked Time to a Date object.
+    #
+    # Examples
+    #
+    #   QuantumLeap::Accelerator.date
+    #   # => #<Date: 1956-09-13 ((2435730j,0s,0n),+0s,2299161j)>
+    #
+    # Returns the mocked current Date.
     def date
       now.to_date
     end
